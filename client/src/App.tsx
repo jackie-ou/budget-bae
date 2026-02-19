e9b9654cc6da75a74a796fecf58b6318e716bc8d
Jackie Ou <jackiehgou@gmail.com>
2026-02-18 21:40:19 -0800

    build(deps): add install and configure react hook form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import { ComponentExample } from "@/components/component-example";
+import { AddExpenseForm } from "./components/AddExpenseForm";
 import { ExpenseList } from "@/components/ExpenseList";
 import expensesData from "@/data/expenses.json";
 
 export function App() {
   return (
     <>
-      <ExpenseList expenses={expensesData}/>
+      <AddExpenseForm />
+      <ExpenseList expenses={expensesData} />
       <ComponentExample />
     </>
   );
