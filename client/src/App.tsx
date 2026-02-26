70775c6c48a7774edb6388a159cb3df55bd25525
jackie-ou <jackiehgou@gmail.com>
2026-02-26 04:53:13 -0800

    build(deps): add storybook

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,12 @@
-import { AddExpenseForm } from "./components/AddExpenseForm";
 import { ExpenseList } from "@/components/ExpenseList";
 import expensesData from "@/data/expenses.json";
-import {
-  Example,
-  ExampleWrapper,
-} from "@/components/example"
 
 export function App() {
   return (
     <>
-      <ExampleWrapper>
-        <Example title="Add Expense Form" className="items-center justify-center">
-          <AddExpenseForm />
-        </Example>
-        <Example title="Expense List" className="items-center">
-          <ExpenseList expenses={expensesData} />
-        </Example>
-      </ExampleWrapper>
+      <section className="grid place-items-center h-screen">
+        <ExpenseList expenses={expensesData} />
+      </section>
     </>
   );
 }
--- client/src/components/ExpenseList.tsx
+++ client/src/components/ExpenseList.tsx
@@ -4,7 +4,7 @@ import type { ExpenseType } from "@/types/expense";
 export function ExpenseList({ expenses }: { expenses: ExpenseType[] }) {
   return (
     <section className="mx-auto w-full max-w-sm flex flex-col">
-      <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">Expenses</h2>
+      <h2 className="scroll-m-20 pb-4 text-3xl font-semibold tracking-tight first:mt-0">Feburary Expenses</h2>
       <div className="expense-list flex flex-col gap-4">
         {expenses.map((item) => (
           <Expense key={item.id} expense={item} />
