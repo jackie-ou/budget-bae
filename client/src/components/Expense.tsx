7841d6bdd2658149a00620fcc354427397daf170
dorothyle <46880658+dorothyle@users.noreply.github.com>
2026-04-04 22:07:30 -0700

    feat(expense-dialog): add edit expense dialog with placeholder form

--- client/src/components/Expense.tsx
+++ client/src/components/Expense.tsx
@@ -1,28 +1,39 @@
+import { AddExpenseForm } from "@/components/AddExpenseForm";
 import { Badge } from "@/components/ui/badge"
 import {
   Card,
-  CardAction,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import type { ExpenseType } from "@/types/expense"
 
 export function Expense({ expense: { title, price, type, date } }: { expense: ExpenseType }) {
   return (
-    <Card size="sm" className="flex flex-row justify-between">
-      <CardHeader>
-        <div className="flex gap-1">
-          <CardTitle>{title}</CardTitle>
-          <Badge>{type}</Badge>
-        </div>
-        <CardDescription>{date}</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <span>${price}</span>
-      </CardContent>
-    </Card>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Card size="sm" className="flex flex-row justify-between cursor-pointer transition-transform duration-300 hover:scale-[1.02] hover:shadow-sm" role="button" tabIndex={0}>
+          <CardHeader>
+            <div className="flex gap-1">
+              <CardTitle>{title}</CardTitle>
+              <Badge>{type}</Badge>
+            </div>
+            <CardDescription>{date}</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <span>${price}</span>
+          </CardContent>
+        </Card>
+      </DialogTrigger>
+      <DialogContent>
+        <AddExpenseForm />
+      </DialogContent>
+    </Dialog>
   )
 }
