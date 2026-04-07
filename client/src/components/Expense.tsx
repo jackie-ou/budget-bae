5f2a71f5e2c0d95f063b4e5635b574e4662e1ea5
dorothyle <46880658+dorothyle@users.noreply.github.com>
2026-04-06 23:12:56 -0700

    feat(expense-details): add expense details to expense dialog

--- client/src/components/Expense.tsx
+++ client/src/components/Expense.tsx
@@ -1,4 +1,4 @@
-import { AddExpenseForm } from "@/components/AddExpenseForm";
+import { ExpenseDetails } from "@/components/ExpenseDetails";
 import { Badge } from "@/components/ui/badge"
 import {
   Card,
@@ -34,7 +34,7 @@ export function Expense({ expense }: { expense: ExpenseType }) {
         </Card>
       </DialogTrigger>
       <DialogContent>
-        <AddExpenseForm />
+        <ExpenseDetails expense={expense}/>
       </DialogContent>
     </Dialog>
   )
