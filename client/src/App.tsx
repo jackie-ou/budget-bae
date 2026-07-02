50dfee860ea6a708a7e2ddc36e06ea9eabaca3c4
dorothyle <46880658+dorothyle@users.noreply.github.com>
2026-07-02 11:26:05 -0700

    feat(supabase): connect to supabase transactions db

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,28 @@
 import { ExpenseList } from "@/components/ExpenseList";
 import expensesData from "@/data/expenses.json";
+import { useState, useEffect } from 'react'
+import { supabase } from "./utils/supabase";
 
 export function App() {
+  const [transactions, setTransactions] = useState([])
+
+  useEffect(() => {
+    async function getTransactions() {
+      const { data: transactions } = await supabase.from('transactions').select()
+
+      if (transactions) {
+        setTransactions(transactions)
+      }
+    }
+
+    getTransactions()
+  }, [])
+
+
   return (
     <>
       <section className="grid place-items-center h-screen">
-        <ExpenseList expenses={expensesData} />
+        <ExpenseList expenses={transactions} />
       </section>
     </>
   );
