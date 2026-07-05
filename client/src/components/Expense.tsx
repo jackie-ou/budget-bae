548b3f7784a175f0a943d11fecca03a408bdfbb9
Dorothy Le <46880658+dorothyle@users.noreply.github.com>
2026-07-05 14:15:10 -0700

    11 connect expense list to transactions database (#25)

--- client/src/components/Expense.tsx
+++ client/src/components/Expense.tsx
@@ -15,21 +15,21 @@ import {
 import type { ExpenseType } from "@/types/expense"
 
 export function Expense({ expense }: { expense: ExpenseType }) {
-  const { title, price, type, date } = expense;
+  const { title, amount, purchase_type, transaction_date } = expense;
 
   return (
     <Dialog>
       <DialogTrigger asChild>
         <Card size="sm" className="flex flex-row justify-between cursor-pointer transition-transform duration-300 hover:scale-[1.02] hover:shadow-sm" role="button" tabIndex={0}>
           <CardHeader>
-            <div className="flex gap-1">
-              <CardTitle>{title}</CardTitle>
-              <Badge>{type}</Badge>
+            <div className="flex gap-2">
+              <CardTitle className="truncate max-w-[200px]">{title}</CardTitle>
+              <Badge>{purchase_type}</Badge>
             </div>
-            <CardDescription>{date}</CardDescription>
+            <CardDescription>{transaction_date}</CardDescription>
           </CardHeader>
           <CardContent>
-            <span>${price}</span>
+            <span>${amount}</span>
           </CardContent>
         </Card>
       </DialogTrigger>
@@ -38,4 +38,6 @@ export function Expense({ expense }: { expense: ExpenseType }) {
       </DialogContent>
     </Dialog>
   )
+
+
 }
